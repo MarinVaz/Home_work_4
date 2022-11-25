3173ee0459cfe878a678bb1af585527dc9f0e340
Marina <errorda@gmail.com>
2022-11-25 12:27:43 +0300

    added task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Без использования функции Math.Pow.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+int numA = ReadInt("Введите число A: ");
+int numB = ReadInt("Введите число B: ");
+Deg(numA, numB);
+
+
+// Функция возведения в степень
+void Deg(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    Console.WriteLine(result);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
